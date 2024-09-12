41d02aa4d199898055d55b318d7018cbc647b775
StoneyDSP <nathanjhood@googlemail.com>
2024-09-12 07:03:32 +0200

    added 'env-dom.d.ts'
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ lib/env-dom.d.ts
@@ -0,0 +1,79 @@
+
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+declare module "*.avif" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.bmp" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.gif" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.webp" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.svg" {
+  import * as React from "react";
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+
+  const src: string;
+  export default src;
+}
+
+// Create types for CSS modules
+declare module "*.module.css" {
+  /** **Experimental:** Import styles that can be used with `react-native-web` components, using the `style` prop. */
+  export const unstable_styles: { readonly [key: string]: object };
+
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.sass" {
+  /** **Experimental:** Import styles that can be used with `react-native-web` components, using the `style` prop. */
+  export const unstable_styles: { readonly [key: string]: object };
+
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.scss" {
+  /** **Experimental:** Import styles that can be used with `react-native-web` components, using the `style` prop. */
+  export const unstable_styles: { readonly [key: string]: object };
+
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+// Allow for css imports, but don't export anything
+declare module "*.css";
+declare module "*.sass";
+declare module "*.scss";
