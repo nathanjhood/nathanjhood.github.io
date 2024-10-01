d4f15fff6c74e541fcd01b76fa006fa587d77161
StoneyDSP <nathanjhood@googlemail.com>
2024-10-01 02:13:03 +0200

    removed unused files
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- .config/esbuild.config.test.ts
+++
@@ -1,8 +0,0 @@
-import * as test from "node:test";
-
-test.describe('esbuild.config.ts', { timeout: 20000 }, (suiteContext) => {
-  test.it('should do something', { timeout: 20000 }, (testContext) => {
-    test.todo('the test', { timeout: 20000 }, (testContext) => {
-    })
-  })
-})
